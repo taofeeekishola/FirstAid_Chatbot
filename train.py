d01f1ad8ace89bf2e27dfcdca24572347ae6add5
Taofeek <taofeekishola1791@gmail.com>
2022-01-10 13:09:26 +0000

    training the model

--- train.py
+++ train.py
@@ -3,8 +3,10 @@ from preprocessing import tokenize, stem, bag_of_words
 import numpy as np
 
 import torch
-import tourh.nn as nn
-from tourh.utils.data import Dataset, DataLoader
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+from model import NeuralNet
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -25,7 +27,6 @@ ignore_words = ['?', '!', '.', ',']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
-print(tags)
 
 x_train = []
 y_train = []
@@ -46,14 +47,51 @@ class ChatDataset(Dataset):
         self.y_data = y_train
 
     def __getitem__(self, index):
-        return self.x_data[idx], self.y_data[idx]
+        return self.x_data[index], self.y_data[index]
     
     def __len__(self):
         return self.n_samples
 
 #Hperparameters
 batch_size = 8
+hidden_size = 8
+output_size = len(tags)
+input_size = len(x_train[0])
+learning_rate = 0.001
+num_epochs = 1000
+
 
 dataset = ChatDataset()
 train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+#loss and optimizer
+criterion = nn.CrossEntropyLoss
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device)
+
+        #forward
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+
+        #backward and optimizer
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch +1) % 100 == 0:
+        print(f'epoch {epoch+1}/{num_epochs}, loss = {loss.item():.4f}')
+
+
+print(f'Final Loss, loss = {loss.item():.4f}')
+
+
+
 

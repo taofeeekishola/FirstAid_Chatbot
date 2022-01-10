27b3f3d5faae785bd57ab36c80f3a1ddd081baa0
Taofeek <taofeekishola1791@gmail.com>
2022-01-10 12:09:36 +0000

    done with preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,10 @@
 import nltk
+import numpy as np
 #nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
-#function for toke
+#function for tokenize
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 
@@ -11,5 +12,14 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(token_sentence, all_words):
-    pass
+    token_sentence = [stem(w) for w in token_sentence]
+
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in token_sentence:
+            bag[idx] = 1.0
+
+    return bag
+
+
 
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import json
 from preprocessing import tokenize, stem, bag_of_words
+import numpy as np
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -17,4 +18,21 @@ for intent in intents['intents']:
         xy.append((w, tag))
 
 ignore_words = ['?', '!', '.', ',']
-print(all_words)
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+print(tags)
+
+x_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    x_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label) #CrossEntropyLoss
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+

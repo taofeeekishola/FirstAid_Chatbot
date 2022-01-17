769b2bfc41de4546cc8e93d6758b155ff7792753
Taofeek <taofeekishola1791@gmail.com>
2022-01-17 03:01:14 +0000

    working on Tkinter

--- app.py
+++ app.py
@@ -1,21 +1,74 @@
-from flask import Flask, render_template, request, jsonify
+from tkinter import *
+from chat import get_response, bot_name
 
-from chat import get_response
+BG_GRAY = "#ABB2B9"
+BG_COLOR = '#17202A'
+TEXT_COLOR = '#EAECEE'
 
-app = Flask(__name__)
+FONT = "Helvetica 14"
+FONT_BOLD = "Helvetica 13 bold"
 
-@app.get("/")
-def index_get():
-    return render_template("base.html")
+class ChatApplication:
 
-@app.post("/predict")
-def predict():
-    text = request.get_json().get("message")
-    #check if text is valid
-    response = get_response(text)
-    message = {"answer": response}
-    return jsonify(message)
+    def __init__(self):
+        self.window = Tk()
+        self._setup_main_window()
+
+    def run(self):
+        self.window.mainloop()
+
+    def _setup_main_window(self):
+        self.window.title("FirstAid Chatbot")
+        self.window.resizable(width=False, height=False)
+        self.window.configure(width=470, height=550, bg=BG_COLOR)
+
+        #head label
+        head_label = Label(self.window, bg=BG_COLOR, fg=TEXT_COLOR,
+                            text= "Welcome", font = FONT_BOLD, pady=10)
+        head_label.place(relwidth=1)  
+
+        #tiny divider
+        line = Label(self.window, width=450, bg=BG_GRAY)
+        line.place(relwidth=1, rely=0.07, relheight=0.012)
+
+        #text widget
+        self.text_widget = Text(self.window, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOR,
+                                font=FONT, padx=5, pady=5)
+        self.text_widget.place(relheight=0.745, relwidth=1, rely=0.08)                 
+        self.text_widget.configure(cursor="arrow", state=DISABLED)
+        
+
+        #scroll bar
+        scrollbar = Scrollbar(self.text_widget)
+        scrollbar.place(relheight=1, relx=0.974)
+        scrollbar.config(command=self.text_widget.yview)
+
+        #bottom label
+        bottom_label = Label(self.window, bg=BG_GRAY, height=80)
+        bottom_label.place(relwidth=1, rely=0.825)
+
+        #message entry box
+        self.msg_entry = Entry(bottom_label, bg="#2C3E50", fg=TEXT_COLOR, font=FONT)
+        self.msg_entry.place(relwidth=0.75, relheight=0.06, rely=0.008, relx=0.011)
+        self.msg_entry.focus()
+        self.msg_entry.bind("<Return>", self._on_enter_pressed)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    def _on_enter_pressed(self, event):
+        msg = self.msg_entry.get()
+        self._insert_message(msg, "You")
+
+    def _insert_message(self, msg, sender):
+        if not msg:
+            return 
+
+        self.msg_entry.delete(0, END)
+        msg1 = f"{sender}: {msg}"
+        self.text_widget.configure(state=NORMAL)
+        self.text_widget.insert(END, msg)
+        self.text_widget.configure(state=DISABLED)
+
+
+if __name__ == '__main__':
+    app = ChatApplication()
+    app.run()
